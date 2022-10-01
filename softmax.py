4de5b5112b16195290967bb5ff06b050c6e0b0f5
yuto <yuto99991@gmail.com>
2022-10-01 13:19:12 +0900

    first commit

---
+++ softmax.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+class SoftMax:
+    def __init__(self):
+        self.output = None
+        self.input_size = None
+        self.dh_du = None
+
+    def forward(self, x):
+        self.input_size = len(x)
+        self.output = np.exp(x) / np.sum(np.exp(x))
+        return self.output
+
+    def backward(self, dx):
+        self.dh_du = np.array(self.input_size, self.input_size)
+        for i in range(0, self.input_size):
+            for j in range(0, self.input_size):
+                self.dh_du[i][j] = - self.output[i] * self.output[j]
+        for i in range(0, self.input_size):
+            self.dh_du[i][i] = self.output[i] * (1 - self.output[i])
+
+    def update(self, batch_n):
+        pass
